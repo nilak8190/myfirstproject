7ab86d663c191da7afeb35effcbafbc66833f49f
nilak8190 <nilak8190@gmail.com>
2026-05-18 14:12:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket"
+    bucket = "azure-aws-oidc-test-bucket-123456"
     key    = "ecr/terraform.tfstate"
     region = "ap-south-1"
   }
